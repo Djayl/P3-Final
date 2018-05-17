308201d7599a191a2c6a263f1e09de683ebdde4c
DjelS <djilali.sakkar@gmail.com>
2018-05-17 15:00:50 +0200

    création de deux nouvelles armes contenues dans le coffre apparaissant aléatoirement, une de guérison pour le mage et une autre d'attaque pour les autres personnages

---
+++ Projet-3/Projet-3/Weapon/Chest/NewWeaponCure.swift
@@ -0,0 +1,17 @@
+//
+//  NewWeaponCure.swift
+//  Projet-3
+//
+//  Created by MacBook DS on 17/05/2018.
+//  Copyright © 2018 Djilali Sakkar. All rights reserved.
+//
+
+import Foundation
+
+// This weapon is in the chest that appears randomly for the magus
+
+class NewWeaponCure: Weapon {
+    init() {
+        super.init(damage: 0, cure: 50)
+    }
+}
