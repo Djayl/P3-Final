a228048d33caf50adbb7b09dae63a76d543f0e2d
DjelS <djilali.sakkar@gmail.com>
2018-05-17 14:49:49 +0200

    création de la classe Sword relative à l'arme du personnage Fighter

---
+++ Projet-3/Projet-3/Sword.swift
@@ -0,0 +1,17 @@
+//
+//  Sword.swift
+//  Projet-3
+//
+//  Created by MacBook DS on 17/05/2018.
+//  Copyright © 2018 Djilali Sakkar. All rights reserved.
+//
+
+import Foundation
+
+// The sword is the weapon of the fighter
+
+class Sword: Weapon{
+    init() {
+        super.init(damage: 10, cure: 0)
+    }
+}
