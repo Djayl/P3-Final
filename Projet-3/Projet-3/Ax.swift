19a4610b29dcb5748fc9ea6c2336a92adb561bb6
DjelS <djilali.sakkar@gmail.com>
2018-05-17 14:45:43 +0200

    création de la classeAx relative à l'arme du personnage Dwarf

---
+++ Projet-3/Projet-3/Ax.swift
@@ -0,0 +1,17 @@
+//
+//  Ax.swift
+//  Projet-3
+//
+//  Created by MacBook DS on 17/05/2018.
+//  Copyright © 2018 Djilali Sakkar. All rights reserved.
+//
+
+import Foundation
+
+// The ax is the weapon of the dwarf
+
+class Ax: Weapon{
+    init() {
+        super.init(damage: 25, cure: 0)
+    }
+}
