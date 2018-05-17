d06b666643b24d27614cf43084c723a10b4f21fb
DjelS <djilali.sakkar@gmail.com>
2018-05-17 14:48:08 +0200

    création de la classe Mace relative à l'arme du personnage Colossus

---
+++ Projet-3/Projet-3/Mace.swift
@@ -0,0 +1,17 @@
+//
+//  Lightning.swift
+//  Projet-3
+//
+//  Created by MacBook DS on 17/05/2018.
+//  Copyright © 2018 Djilali Sakkar. All rights reserved.
+//
+
+import Foundation
+
+// The mace is the weapon of the colossus
+
+class Mace: Weapon{
+    init() {
+        super.init(damage: 5, cure: 0)
+    }
+}
