d9a34fe6c2a74eca11d58eba3e2f8d87170db64b
DjelS <djilali.sakkar@gmail.com>
2018-05-17 15:02:52 +0200

    ajout d'un nouveau personnage et de son arme pour le bonus, il s'agit du spectre

---
+++ Projet-3/Projet-3/Weapon/Lightning.swift
@@ -0,0 +1,17 @@
+//
+//  Lightning.swift
+//  Projet-3
+//
+//  Created by MacBook DS on 17/05/2018.
+//  Copyright © 2018 Djilali Sakkar. All rights reserved.
+//
+
+import Foundation
+
+// The lightning is the weapon of the spectrum
+
+class Lightning: Weapon{
+    init() {
+        super.init(damage: 150, cure : 0)
+    }
+}
