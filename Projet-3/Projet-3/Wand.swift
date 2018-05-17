24289af08e31581a079ce590ac600911391075a5
DjelS <djilali.sakkar@gmail.com>
2018-05-17 14:50:51 +0200

    création de la classe Wand realtive à l'arme du personnage Magus

---
+++ Projet-3/Projet-3/Wand.swift
@@ -0,0 +1,17 @@
+//
+//  Wand.swift
+//  Projet-3
+//
+//  Created by MacBook DS on 17/05/2018.
+//  Copyright © 2018 Djilali Sakkar. All rights reserved.
+//
+
+import Foundation
+
+// The wand is the weapon of the magus
+
+class Wand: Weapon{
+    init() {
+        super.init(damage: 0, cure : 10)
+    }
+}
